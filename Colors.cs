fcfd9c29ee2d2364dfc3419d47d050e4d73e2ee6
Dexter Arnold <arnolddexj@gmail.com>
2026-09-10 12:41:59 -0400

    fixed formatting

--- Colors.cs
+++ Colors.cs
@@ -1,14 +1,12 @@
-namespace Mille
-{
-    class Colors
-    {
-        public const string Reset = "\u001b[0m";
-        public const string Bold = "\u001b[1m";
-        public const string Red = "\u001b[31m";
-        public const string Green = "\u001b[32m";
-        public const string Yellow = "\u001b[33m";
-        public const string Cyan = "\u001b[36m";
-        public const string Magenta = "\u001b[35m";
-        public const string DarkGray = "\u001b[90m";
-        }
+namespace Mille {
+	class Colors {
+		public const string Reset = "\u001b[0m";
+		public const string Bold = "\u001b[1m";
+		public const string Red = "\u001b[31m";
+		public const string Green = "\u001b[32m";
+		public const string Yellow = "\u001b[33m";
+		public const string Cyan = "\u001b[36m";
+		public const string Magenta = "\u001b[35m";
+		public const string DarkGray = "\u001b[90m";
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
 ﻿using System.Drawing;
 
-namespace Mille
-{
-    public class Program
-    {
-        static void Main()
-        {
-        Console.WriteLine($"{Colors.Red} Welcome to Mille");
-        Console.WriteLine("");
-        Console.WriteLine($"{Colors.Reset}Theres nothing here yet...");
-        }
-    }
+namespace Mille {
+	public class Program {
+		static void Main() {
+			Console.WriteLine($"{Colors.Red} Welcome to Mille");
+			Console.WriteLine("");
+			Console.WriteLine($"{Colors.Reset}Theres nothing here yet...");
+		}
+	}
 }
