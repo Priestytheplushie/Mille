afe4642860fd0d9c8658e70a9d483f477a5b9ab8
Priestytheplushie <priestyinc@gmail.com>
2026-09-11 12:36:44 -0400

    Add file opening logic

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 namespace Mille {
 	public class Program {
-		static void Main() {
-			Console.WriteLine($"{Colors.Red} Welcome to Mille");
-			Console.WriteLine("");
-			Console.WriteLine($"{Colors.Reset}Theres nothing here yet...");
+		static void Main(string[] args) {
+			if (args.Length > 0) {
+				string path = args[0];
+				string content = File.ReadAllText(path); 
+				string[] split = content.Split("\n");
+				SortedDictionary<int, string> contents = new SortedDictionary<int, string>();
+				for(int i = 0; i < split.Length; i++) {
+					contents.Add(i,split[i]);
+				}
+			}
+			else {
+				Console.WriteLine("Avaliable Arguements:");
+				Console.WriteLine("<filename> The file to open");
+			}
 		}
 	}
 }
