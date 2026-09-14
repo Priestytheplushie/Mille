b652055834c44505ca8698173f4cdd350a96f66e
Priestytheplushie <priestyinc@gmail.com>
2026-09-14 18:49:29 -0400

    add more colors

--- Colors.cs
+++ Colors.cs
@@ -1,16 +1,41 @@
 namespace Mille {
-	class Colors {
-		public const string Reset = "\u001b[0m";
-		public const string Red = "\u001b[31m";
-		public const string Green = "\u001b[32m";
-		public const string Yellow = "\u001b[33m";
-		public const string Cyan = "\u001b[36m";
-		public const string Magenta = "\u001b[35m";
-		public const string Orange = "\u001b[38;5;214m";
-		public const string DarkGray = "\u001b[90m";
-		public const string Brown = "\u001b[38;5;94m";
-		public const string Peach = "\x1b[38;2;255;229;180m";
-		public const string Marroone = "\x1b[38;2;140;0;0m";
-		public const string Teal = "\x1b[38;5;30m";
-	}
+    public static class Colors {
+        public const string Reset = "\u001b[0m";
+        public const string Black = "\u001b[30m";
+        public const string Red = "\u001b[31m";
+        public const string Green = "\u001b[32m";
+        public const string Yellow = "\u001b[33m";
+        public const string Blue = "\u001b[34m";
+        public const string Magenta = "\u001b[35m";
+        public const string Cyan = "\u001b[36m";
+        public const string White = "\u001b[37m";
+        public const string DarkGray = "\u001b[90m";
+        public const string BrightRed = "\u001b[91m";
+        public const string BrightGreen = "\u001b[92m";
+        public const string BrightYellow = "\u001b[93m";
+        public const string BrightBlue = "\u001b[94m";
+        public const string BrightMagenta = "\u001b[95m";
+        public const string BrightCyan = "\u001b[96m";
+        public const string BrightWhite = "\u001b[97m";
+        public const string Orange = "\u001b[38;5;214m";
+        public const string Brown = "\u001b[38;5;94m";
+        public const string Teal = "\u001b[38;5;30m";
+        public const string Purple = "\u001b[38;5;129m";
+        public const string Pink = "\u001b[38;5;206m";
+        public const string Lime = "\u001b[38;5;118m";
+        public const string Gold = "\u001b[38;5;220m";
+        public const string Indigo = "\u001b[38;5;54m";
+        public const string Peach = "\u001b[38;2;255;229;180m";
+        public const string Maroon = "\u001b[38;2;140;0;0m";
+        public const string Lavender = "\u001b[38;2;230;230;250m";
+        public const string Mint = "\u001b[38;2;189;252;201m";
+        public const string Coral = "\u001b[38;2;255;127;80m";
+        public const string Olive = "\u001b[38;2;128;128;0m";
+        public const string SoftRed = "\u001b[38;2;241;76;76m";
+        public const string SoftGreen = "\u001b[38;2;115;201;145m";
+        public const string Sand = "\u001b[38;2;220;200;140m";
+        public const string Plum = "\u001b[38;2;221;160;221m";
+        public const string MutedGray = "\u001b[38;2;106;115;125m";
+        public const string Charcoal = "\u001b[38;2;40;44;52m";
+    }
 }
