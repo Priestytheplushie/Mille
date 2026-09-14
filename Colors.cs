4e5b26583a304d2d64b576b601a69ae3f30fe0da
Priestytheplushie <priestyinc@gmail.com>
2026-09-14 13:03:19 -0400

    WIP: Add more colors

--- Colors.cs
+++ Colors.cs
@@ -1,12 +1,16 @@
 namespace Mille {
 	class Colors {
 		public const string Reset = "\u001b[0m";
-		public const string Bold = "\u001b[1m";
 		public const string Red = "\u001b[31m";
 		public const string Green = "\u001b[32m";
 		public const string Yellow = "\u001b[33m";
 		public const string Cyan = "\u001b[36m";
 		public const string Magenta = "\u001b[35m";
+		public const string Orange = "\u001b[38;5;214m";
 		public const string DarkGray = "\u001b[90m";
+		public const string Brown = "\u001b[38;5;94m";
+		public const string Peach = "\x1b[38;2;255;229;180m";
+		public const string Marroone = "\x1b[38;2;140;0;0m";
+		public const string Teal = "\x1b[38;5;30m";
 	}
 }
